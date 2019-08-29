23f4258553ae22b23c06b178d68f1f450bb00289
Till Brede <tb@symcon.de>
2019-08-29 14:46:35 +0200

    applied cs fixer style

--- Umbenenner/module.php
+++ Umbenenner/module.php
@@ -1,37 +1,39 @@
 <?php
 
+declare(strict_types=1);
+
     class Umbenenner extends IPSModule
     {
         public function Create()
         {
             //Never delete this line!
             parent::Create();
-            
-            $this->RegisterPropertyInteger("SourceVariable", 0);
 
-            $this->RegisterVariableString("Name", "Name");
-            $this->EnableAction("Name");
+            $this->RegisterPropertyInteger('SourceVariable', 0);
+
+            $this->RegisterVariableString('Name', 'Name');
+            $this->EnableAction('Name');
         }
-    
+
         public function ApplyChanges()
         {
-            
+
             //Never delete this line!
             parent::ApplyChanges();
-            
-            SetValue($this->GetIDForIdent("Name"), IPS_GetName($this->ReadPropertyInteger("SourceVariable")));
+
+            SetValue($this->GetIDForIdent('Name'), IPS_GetName($this->ReadPropertyInteger('SourceVariable')));
         }
 
         public function RequestAction($Ident, $Value)
         {
             switch ($Ident) {
-                case "Name":
-                    IPS_SetName($this->ReadPropertyInteger("SourceVariable"), $Value);
-                    SetValue($this->GetIDForIdent("Name"), $Value);
+                case 'Name':
+                    IPS_SetName($this->ReadPropertyInteger('SourceVariable'), $Value);
+                    SetValue($this->GetIDForIdent('Name'), $Value);
                     break;
 
                 default:
-                    throw new Exception("Invalid Ident");
+                    throw new Exception('Invalid Ident');
             }
         }
     }
