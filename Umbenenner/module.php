6ba43e11f6937503fe424093efd8d0ecf4d08151
TillBrede <51406030+TillBrede@users.noreply.github.com>
2019-09-02 22:36:16 +0200

    applied cs fixer style (PHP-69) (#3)

--- Umbenenner/module.php
+++ Umbenenner/module.php
@@ -1,43 +1,39 @@
-<?
+<?php
 
-	class Umbenenner extends IPSModule
-	{
-		
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
-			
-			$this->RegisterPropertyInteger("SourceVariable", 0);
+declare(strict_types=1);
 
-			$this->RegisterVariableString("Name", "Name");
-			$this->EnableAction("Name");
+    class Umbenenner extends IPSModule
+    {
+        public function Create()
+        {
+            //Never delete this line!
+            parent::Create();
 
-		}
-	
-		public function ApplyChanges()
-		{
-			
-			//Never delete this line!
-			parent::ApplyChanges();
-			
-			SetValue($this->GetIDForIdent("Name"), IPS_GetName($this->ReadPropertyInteger("SourceVariable")));
-			
-		}
+            $this->RegisterPropertyInteger('SourceVariable', 0);
 
-        public function RequestAction($Ident, $Value) {
+            $this->RegisterVariableString('Name', 'Name');
+            $this->EnableAction('Name');
+        }
 
+        public function ApplyChanges()
+        {
+
+            //Never delete this line!
+            parent::ApplyChanges();
+
+            SetValue($this->GetIDForIdent('Name'), IPS_GetName($this->ReadPropertyInteger('SourceVariable')));
+        }
+
+        public function RequestAction($Ident, $Value)
+        {
             switch ($Ident) {
-                case "Name":
-                    IPS_SetName($this->ReadPropertyInteger("SourceVariable"), $Value);
-                    SetValue($this->GetIDForIdent("Name"), $Value);
+                case 'Name':
+                    IPS_SetName($this->ReadPropertyInteger('SourceVariable'), $Value);
+                    SetValue($this->GetIDForIdent('Name'), $Value);
                     break;
 
                 default:
-                    throw new Exception("Invalid Ident");
+                    throw new Exception('Invalid Ident');
             }
         }
-	
-	}
-
-?>
+    }
