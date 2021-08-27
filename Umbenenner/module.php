05c01f3dc600e0d86011989fccf833c6d6897a7d
Till Brede <tb@symcon.de>
2021-08-27 15:18:32 +0200

    added list and documentation

--- Umbenenner/module.php
+++ Umbenenner/module.php
@@ -10,30 +10,51 @@ declare(strict_types=1);
             parent::Create();
 
             $this->RegisterPropertyInteger('SourceVariable', 0);
-
-            $this->RegisterVariableString('Name', 'Name');
-            $this->EnableAction('Name');
+            $this->RegisterPropertyString('Objects', '[]');
         }
 
         public function ApplyChanges()
         {
-
             //Never delete this line!
             parent::ApplyChanges();
 
-            SetValue($this->GetIDForIdent('Name'), IPS_GetName($this->ReadPropertyInteger('SourceVariable')));
+            //Register variables from list
+            $objects = json_decode($this->ReadPropertyString('Objects'), true);
+            for ($i = 0; $i < count($objects); $i++) {
+                $objectID = $objects[$i]['ObjectID'];
+                $this->RegisterVariableString($objectID, IPS_GetLocation($objectID), '', 0);
+                $this->EnableAction($objectID);
+                $this->SetValue($objectID, IPS_GetName($objects[$i]['ObjectID']));
+            }
+
+            $childrenIDs = IPS_GetChildrenIDs($this->InstanceID);
+            $childInObjects = function ($childIdent) use ($objects)
+            {
+                foreach ($objects as $object) {
+                    if ($object['ObjectID'] == $childIdent) {
+                        return true;
+                    }
+                }
+                return false;
+            };
+
+            //Remove objects not present in list
+            foreach ($childrenIDs as $childID) {
+                $childIdent = IPS_GetObject($childID)['ObjectIdent'];
+                if (!$childInObjects($childIdent)) {
+                    $this->UnregisterVariable($childIdent);
+                }
+            }
         }
 
         public function RequestAction($Ident, $Value)
         {
-            switch ($Ident) {
-                case 'Name':
-                    IPS_SetName($this->ReadPropertyInteger('SourceVariable'), $Value);
-                    SetValue($this->GetIDForIdent('Name'), $Value);
-                    break;
-
-                default:
-                    throw new Exception('Invalid Ident');
+            preg_match('/[0-9]{5}/m', $Ident, $matches);
+            if ($matches) {
+                $objects = json_decode($this->ReadPropertyString('Objects'), true);
+                IPS_SetName($Ident, $Value);
+                IPS_SetName($this->GetIDForIdent($Ident), IPS_GetLocation($Ident));
+                $this->SetValue($Ident, $Value);
             }
         }
     }
